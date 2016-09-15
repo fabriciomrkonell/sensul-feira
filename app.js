3a8781c497d79365cc1e83992581658acf3a342c
Fabricio Ronchi <fabricioronchii@gmail.com>
2016-09-14 21:05:33 -0300

    Up

--- app.js
+++ app.js
@@ -17,8 +17,14 @@ var client = mqtt.connect('mqtt://test.mosquitto.org');
 
 client.on('connect', function () {
 	console.log('Conectado!');
+	client.subscribe('automation');
+	client.publish('automation', 'Hello mqtt');
 });
 
 client.on('automation', function (topic, message) {
   console.log('Mensagem recebida!');
+});
+
+client.on('message', function (topic, message) {
+  console.log('Mensagem recebida em mensagem!');
 });
